27dc631abea27b2db9b34753d8ed27ea13fdd797
Arman <aisenovdev@gmail.com>
2022-01-11 16:35:15 +0500

    task was updated, lesson_2_10

--- script.js
+++ script.js
@@ -11,14 +11,30 @@ let appData = {
   saving: false
 };
 
-let b1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-  b2 = +prompt('Во сколько обойдется?', ''),
-  b3 = prompt('Введите обязательную статью расходов в этом месяце', ''),
-  b4 = +prompt('Во сколько обойдется?', '');
+for (let i = 0; i < 2; i++){
+  let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+      b = +prompt('Во сколько обойдется?', '');
+  if ((typeof (a)) === 'string' && (typeof (a)) != null && (typeof (b)) != null 
+    && a != '' && b != '' && a.length < 50) {
+    console.log('done');
+    appData.expenses[a] = b;
+  } else {
+    console.log('bad result');
+    i--;
+  }
+};
 
-appData.expenses.b1 = b2,
-appData.expenses.b3 = b4;
+appData.moneyPerDay = appData.budget / 30;
 
-alert(appData.budget/30);
+alert('Ежедневный бюджет: ' + appData.moneyPerDay);
 
-
+if (appData.moneyPerDay < 100) {
+  console.log('Минимальный уровень достатка');
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+  console.log('Средний уровень достатка');
+} else if (appData.moneyPerDay > 2000) {
+  console.log('Высокий уровень достатка');
+} else {
+  console.log('Произошла ошибка');
+  
+}
